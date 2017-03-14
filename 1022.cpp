eaa70f6ffb5efed9fd690c30b2722b183dd08766
fanliyong <fanliyong@localhost.localdomain>
2017-03-14 18:32:05 +0800

    not ok

--- 1022.cpp
+++ 1022.cpp
@@ -4,50 +4,61 @@
 using namespace  std;
 void fan(string a,string b,int cs)
 {
-    stack<char>stk(9);
-    char* achar=a.c_str();
-    char* bchar=b.c_str();
-    bool flag1[n];
+    stack<char>stk;
+    const char*achar=a.c_str();
+    const char*bchar=b.c_str();
+    bool flag1[cs]={true};
+    bool flag3[cs]={true};
     bool flag2=true;
-
-//     for(int i=0;i<cs;i++)
-//     {   
-//         if(i==0)
-//             stk.push(achar[0]);
-//         if(stk.top()==bchar[i])
-//         {
-//             stk.pop();
-//             flag1[i]=true;
-//         }
-//         else
-//         {
-//             stk.push(achar[i])
-//             flag1[i]=false;           
-//         }
-//     }
-//     for(int j=n;j>;j--)
-//     {
-//         if(stk.top==bchar[j])
-//             stk.pop();
-//         else
-//             flag2=false;
-//     }
-//     if(flag2==true)
-//     {
-//         cout<<"No."<<endl;
-//     }
-//     else
-//     {
-//         cout<<"Yes."<<endl;
-//         for(int k=0;k<cs;k++)
-//         {
-//             if(flag1[k])
-//                 cout<<"in"<<endl;
-//             else
-//                 cout<<"out"<<endl;
-//         }        
-//     }
-// }
+    int x=0;
+    for(int i=0;i<cs;i++)
+    {
+        stk.push(achar[i]);
+        if(stk.top()==bchar[x])
+        {
+            stk.pop();
+            flag1[i]=false;
+            flag3[i]=true;
+            x++;
+        }
+    }
+    for(int j=x;j<cs;j++)
+    {
+        if(stk.top()==bchar[j])
+        {
+            stk.pop();
+            flag1[j]=true;
+            flag3[j]=false;
+        }
+        else
+        {
+            flag2=false;
+            break;
+        }
+    }
+    if(flag2==false)
+    {
+        cout<<"No."<<endl;
+    }
+    else
+    {
+        cout<<"Yes."<<endl;
+        for(int k=0;k<cs;k++)
+        {
+            if(flag1[k])
+                cout<<"in"<<endl;
+            else
+                cout<<"out"<<endl;
+        }    
+        for(int l=0;l<cs;l++)
+        {
+            if(flag3[l])
+                cout<<"in"<<endl;
+            else
+                cout<<"out"<<endl;
+        }    
+    }
+}
 int main()
 {
     int n;
@@ -56,7 +67,7 @@ int main()
     while(cin>>n)
     {
         cin>>astring;
-        cin>>bstirng;
+        cin>>bstring;
         fan(astring,bstring,n);
         cout<<"FINISH"<<endl;
     }
