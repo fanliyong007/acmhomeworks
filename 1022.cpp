4e96725351b97ffe165149eb6849035ece90add1
fanliyong <fanliyong@localhost.localdomain>
2017-03-16 10:17:04 +0800

    ok

--- 1022.cpp
+++ 1022.cpp
@@ -1,76 +1,57 @@
+//注意元素5 12342 24321
 #include<iostream>
 #include<stack>
-#include<cstring>
 #define max 100
-using namespace  std;
+using namespace std;
 void fan(char achar[],char bchar[],int cs)
 {
+    int flag[max];
+    for(int j=0;j<cs*2;j++)
+        flag[j]=true;
+    int x=0,y=0,z=0;
     stack<char>stk;
-    bool flag1[max];
-    bool flag2=true;
-    int x=0;
-    int y=0;
-    for(int l=0;l<cs*2;l++)
-        flag1[l]=true;//初始化所有元素为进
-    for(int i=0;i<cs;i++)
+    while(x<=cs)
     {
-        stk.push(achar[i]);//把元素压进来
-        if(stk.top()==bchar[x]&&(i!=cs-1))//如果顶层元素与序列b的首位元素相同那么把它弹出去，并标记此元素为进出
+        if(stk.empty())//第一次是空栈所以压入
+        {
+            stk.push(achar[x]);
+            x++;z++;
+        }
+        if(!stk.empty()&&stk.top()==bchar[y])//如果最顶上元素等于bchar[y]的元素
         {
             stk.pop();
-            x++;//此值用于标记进出元素位置
-            y++;
-            flag1[y]=false;
-            y++;
+            flag[z]=false;
+            y++;z++;
         }
-        else
+        if(!stk.empty()&&stk.top()!=bchar[y])
         {
-            y++;//否则就定位到下一个元素
-        }
+            stk.push(achar[x]);
+            x++;z++;
+        }   
     }
-    for(int j=x;j<cs;j++)
-    {
-        if(bchar[j]==stk.top())//如果现在顶层元素与序列b剩下的元素相同那么弹出并且标记出去
-        {
-            stk.pop();
-            flag1[y]=false;
-            y++;
-        }
-        else
-        {
-            flag2=false;
-            break;
-        }
-    }
-    if(flag2==false)
-    {
-        cout<<"No."<<endl;
-    }
-    else
+    if(stk.empty())
     {
         cout<<"Yes."<<endl;
-        for(int k=0;k<cs*2;k++)
+        for(int i=0;i<cs*2;i++)
         {
-            if(flag1[k])
-               cout<<"in"<<endl;
+            if(flag[i])
+                cout<<"in"<<endl;
             else
-                cout<<"out"<<endl;   
-        }                   
+                cout<<"out"<<endl;
+        }
     }
+    else
+        cout<<"No."<<endl;
 }
 int main()
 {
-    int n;
+    int n=0;
     while(cin>>n)
     {
         char a[n];
         char b[n];
-        cin>>a;
-        cin>>b;
+        cin>>a>>b;
         fan(a,b,n);
         cout<<"FINISH"<<endl;
     }
-    return 0;
 }
-//方法错误，如果有序号相同的火车那么就会出错
-//如5 12342 24321
