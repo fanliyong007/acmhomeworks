59ed552a588325e6d685947762990932ff56aaed
unknown <>
2017-03-31 20:08:38 +0800

    ok

--- test.cpp
+++ test.cpp
@@ -15,34 +15,40 @@ int main()
         for(int k=sqrt(i);k>0;k--)
         {
             if(i%k==0)
-                flag=false;
-        }
-        int copyone=i;
-        int copytwo=i;
-        while(copyone!=0&&copytwo!=0)
-        {
-            stk.push(copyone%10);
-            copyone=copyone/10;
-            que.push(copytwo%10)
-            copytwo=copytwo/10;
-        }
-        while(!stk.empty())
-        {
-            if(stk.top()!=que.front())
             {
                 flag=false;
                 break;
+            }               
+        }
+        if(flag)
+        {
+            int copyone=i;
+            int copytwo=i;
+            while(copyone!=0&&copytwo!=0)
+            {
+                stk.push(copyone%10);
+                copyone=copyone/10;
+                que.push(copytwo%10);
+                copytwo=copytwo/10;
             }
-            stk.pop();
-            que.pop();
-        }
-        while(!stk.empty())
-        {
-            stk.pop();
-        }
-        while(!que.empty())
-        {
-            que.pop();
+            while(!stk.empty())
+            {
+                if(stk.top()!=que.front())
+                {
+                    flag=false;
+                    break;
+                }
+                stk.pop();
+                que.pop();
+            }
+            while(!stk.empty())
+            {
+                stk.pop();
+            }
+            while(!que.empty())
+            {
+                que.pop();
+            }
         }
         if(flag)
             cout<<i<<",";
