2935e31bf4c4287461f0ded4f8c62cf7be2759e2
fanliyong007 <fanliyong007@hotmail.com>
2017-12-15 21:19:18 +0800

    ok

--- 1164.cpp
+++ 1164.cpp
@@ -1,34 +1,35 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+bool isprime(int num)
+{
+    bool flag = true;
+    for(int k=num-1;k>1;k--)
+    {
+        if(num%k==0)
+        {
+            flag=false;
+            break;
+        }
+    }
+     return flag;
+}
 int main()
 {
     int x=0;
-    bool prime[10000];
-    memset(prime,true,sizeof(prime));
-    for(int i=3;i<10000;i++)
-    {
-        for(int k=i-1;k>1;k--)
-        {
-            if(i%k==0)
-            {
-                prime[i]=false;
-                break;
-            }
-        }
-    }
+ 
     while(cin>>x)
     {
          for(int i=2;i<=x;i++)
          {
-             if((x%i==0)&&prime[i])
+             if((x%i==0)&&isprime(i))
              {
                 x/=i;
                 if(x!=1)
                     cout<<i<<"*";
                 else
                     cout<<i;    
-                i=2;
+                i=1;
              }
          }
          cout<<endl;
---
+++ 2708.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char x;
+    cin >> x;
+    cout << (int)x;
+    cin >> x;
+    return 0;
+}
