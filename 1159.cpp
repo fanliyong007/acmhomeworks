5d34d1c01b5e5ed4e95aa7634dc2c7a1b9131f19
fanliyong007 <fanliyong007@hotmail.com>
2018-03-08 20:21:10 +0800

    ok

--- 1159.cpp
+++ 1159.cpp
@@ -2,21 +2,27 @@
 #include<cstdio>
 #include<cstring>
 using namespace std;
+int ans[1001][1001];
 int main()
 {
     string X, Y;
     while(cin>>X>>Y)
     {
-        int max = 0, tmp = 0,flag=1;
-        int len1 = X.length(),len2=Y.length();
+        int len1 = X.length();
+        int len2 = Y.length();
+        for (int i = 0; i <= len1;i++)
+            ans[i][0] = 0;
+        for (int i = 0; i <= len2;i++)
+            ans[0][i] = 0;
         for (int i = 1; i <= len1;i++)
-        {
-            for (int j = flag; j <= len2;j++)
+            for (int j = 1; j <= len2;j++)
             {
-             
+                if(X[i-1]==Y[j-1])
+                    ans[i][j] = ans[i - 1][j - 1] + 1;
+                else
+                    ans[i][j] = ans[i][j - 1] > ans[i - 1][j] ? ans[i][j - 1] : ans[i - 1][j];
             }
-        }
+        cout << ans[len1][len2] << endl;
     }
     return 0;
 }
-
--- test.cpp
+++ test.cpp
@@ -3,14 +3,6 @@
 using namespace std;
 int main()
 {
-    
-      while(true)
-      {
-          string a;
-          cin >> a;
-          cout << a.length()<<a[2]<<endl;
-          
-      }
     // -2147483648 < 2147483647 ? printf("true") : printf("false");
     // cin.get();
     // return 0;
