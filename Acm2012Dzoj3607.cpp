9113f1916768c2dff54ba04ecca3741b91115509
fanliyong007 <fanliyong007@hotmail.com>
2018-03-28 15:21:13 +0800

    let me think a minutes

--- Acm2012Dzoj3607.cpp
+++ Acm2012Dzoj3607.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct customer
+{
+    double time;
+    double price;
+};
 int main()
 {
+    struct customer cs[1008];
+    int T = 0;
+    int n = 0;
+    int pi[1008];
+    int ti[1008];
+    int sum = 0;
+    scanf("%d", &T);
+    while(T--)
+    {
+        memset(pi, 0, sizeof(pi));
+        memset(ti, 0, sizeof(ti));
+        scanf("%d", &n);
+        for (int i = 0; i < n;i++)
+            scanf("%d", &pi[i]);
+        for (int i = 0; i < n;i++)
+            scanf("%d", &ti[i]);
+        for (int i = 0; i < n;i++)
+        {
+            
+        }
+
+        printf("%.06lf %.06lf", );
+    }
     return 0;
 }
