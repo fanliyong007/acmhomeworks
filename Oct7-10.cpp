1f6d9998626c52427f96057c5db2232263778938
fanliyong007 <fanliyong007@hotmail.com>
2018-10-23 20:28:22 +0800

    注意边界条件

--- Oct7-10.cpp
+++ Oct7-10.cpp
@@ -1,8 +1,6 @@
+#define local
 #include<bits/stdc++.h>
 using namespace std;
-int inOrder[58];
-int postOrder[58];
-int N;
 typedef struct TreeNode *BinTree;
 typedef BinTree Position;
 struct TreeNode
@@ -11,56 +9,35 @@ struct TreeNode
     BinTree Left;
     BinTree Right;
 };
-BinTree root = NULL;
-BinTree cur = NULL;
-void addNode(int x,bool flag)
+BinTree root = new TreeNode();
+BinTree FindRoot(int *a,int *b,int n)
 {
-    BinTree tmp;
-    if(!root)
-    {
-        root->data = x;
-        cur = root;
-    }
-    else
-    {
-        tmp->data = x;
-        if(flag)
-        {
-            cur->Left = tmp;
-            cur = tmp;
-        }
-        else
-        {
-            cur->Right = tmp;
-            cur = tmp;
-        }
-        
-    }
-}
-void FindRoot(int begin,int end,bool flag)
-{
-    if(end<0)
-        return;
-    for (int i = begin; i < end;i++)
-    {
-        if(inOrder[i]==postOrder[end])
-        {
-            addNode(postOrder[end],flag);
-            FindRoot()
-            FindRoot()
+    if(n <= 0) return NULL;
+    BinTree tmp = new TreeNode();
+    int findroot = a[n - 1];
+    int i;
+    for(i = 0; i < n; i++) {
+        if(b[i] == findroot) {
+            break;
         }
     }
+    tmp->data = findroot;
+    tmp->Left = FindRoot(a, b, i);
+    tmp->Right = FindRoot(a + i, b + i + 1, n - i - 1);
+    return tmp;
 }
 void Levelprint()
 {
     queue<BinTree> Q;
     Q.push(root);
-    BinTree T;
     while(!Q.empty())
     {
-        T=Q.front();
+        BinTree T=Q.front();
         Q.pop();
-        printf("%d\n",T->data);
+        if(T==root)
+            cout << T->data;
+        else
+            cout << " " << T->data;
         if(T->Left)
             Q.push(T->Left);
         if(T->Right)
@@ -69,6 +46,9 @@ void Levelprint()
 }
 int main()
 {
+    int inOrder[38];
+    int postOrder[38];
+    int N;
     cin >> N;
     for (int i = 0; i < N;i++)
     {
@@ -78,7 +58,11 @@ int main()
     {
         cin >> inOrder[i];
     }
-    FindRoot(0,N,true);
+    root=FindRoot(postOrder, inOrder, N);
     Levelprint();
+    cout << endl;
+    #ifdef local
+    system("pause");
+    #endif
     return 0;
 }
