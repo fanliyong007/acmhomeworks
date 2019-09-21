f711055db9f8cb96fa6a60c75585e8683b0a7472
fanliyong007 <fanliyong007@hotmail.com>
2019-09-21 08:22:00 +0800

    xixi

--- test.cpp
+++ test.cpp
@@ -1,20 +1,9 @@
-#define local
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
+#include <stdio.h>
 
-    string s = "I love you!";
-    for( float y = 1.3 ; y >= -1.1 ; y -= 0.06 ){
-        int index = 0;
-        for( float x = -1.2 ; x <= 1.2 ; x += 0.025 )
-            if( pow((x*x+y*y-1.0),3) - x*x*y*y*y <= 0.0 )
-                cout<<s[(index++)%s.size()];
-            else
-                cout<<' ';
-        cout<<endl;
-    }
-    #ifdef local
-    system("pause");
-    #endif
-    return 0;
+int main()
+{
+   for(int i=1;i<=3;i++)
+   {
+      printf("*");
+   }
 }
